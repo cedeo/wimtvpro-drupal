725cc2e0a6ddc81efddd8b580f1b29f3e5e9cf17
Walter Danilo Galante <walterdangalante@gmail.com>
2014-01-28 14:33:57 +0100

    Fixed thumb size

--- templates/playlist_row.php
+++ templates/playlist_row.php
@@ -1,10 +1,6 @@
 <li id="<?php echo $contentid ?>">
-
-
-        <a class="viewThumbPlaylist" title="<?php echo t("Preview Video");?>" href="#" id="<?php echo $preview ?>">
-        <?php echo $thumbnail ?>
-        </a><br/>
-        <b class="title"><?php echo $title ?></b>
-
- 
+    <a class="viewThumbPlaylist" title="<?php echo t("Preview Video");?>" href="#" id="<?php echo $preview ?>">
+    <?php echo $thumbnail ?>
+    </a><br/>
+    <b class="title"><?php echo $title ?></b>
 </li>
