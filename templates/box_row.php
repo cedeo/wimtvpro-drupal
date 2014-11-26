6d378c9e2fb85898880c31cc3fb8a3e24e1ff700
gtropea <Giuseppe Tropea>
2014-11-26 16:35:53 +0100

    disalign
    now thumbnail view in add videos to content is aligned

--- templates/box_row.php
+++ templates/box_row.php
@@ -3,11 +3,15 @@
         <a class='addThumb' href='#' id='<?php echo $publish_id ?>'><?php echo t("Add") ?></a>
         <a class='removeThumb' href='#' id='<?php echo $publish_id ?>'><?php echo t("Remove") ?></a>
     <?php } ?>
-    <a class="viewThumbsPublic" title="<?php echo $title;?>" href="#" id="<?php echo $preview ?>">
-    <?php echo $thumbnail ?>
-    <b class="title"><?php echo $title ?></b>
+
+    <a class="viewThumbsPublic" title="<?php echo $title; ?>" href="#" id="<?php echo $preview ?>">
+        <div class="viewThumbsPublicContainer">
+            <?php echo $thumbnail ?>
+        </div>        
+        <b class="title"><?php echo $title ?></b>
     </a>
+
     <?php if ($is_field) { ?>
         <p>W <input style='width:20px;' maxweight='3' class='w' type='text' value='<?php echo variable_get("widthPreview") ?>'>px  -  H <input style='width:20px;' maxweight='3' class='h' type='text' value='<?php echo variable_get("heightPreview") ?>'>px</p>
-    <?php } ?>
+        <?php } ?>
 </li>
